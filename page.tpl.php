d6b30793748f97da6752d42097f11609f029a989
Dustin Perzanowski <perzanowski.4@osu.edu>
2013-11-06 13:59:07 -0500

    update OSU navbar

--- page.tpl.php
+++ page.tpl.php
@@ -74,7 +74,27 @@ $no_title_node_types = array("qs_events", "qs_people");
 
 <body class="<?php print $body_classes; ?>">
 
-  <?php require_once("navbar/osu-navbar-b-custom.php"); ?>
+  <div id="osu_navbar" role="navigation">
+      <div class="container">
+        <h2 class="osu-semantic">Ohio State nav bar</h2>
+      <a href="#page-content" id="skip" class="osu-semantic">Skip to main content</a>
+        <div class="univ_info">
+          <p class="univ_name"><a href="http://osu.edu" title="The Ohio State University">The Ohio State University</a></p>
+        </div><!-- /univ_info -->
+        <div class="univ_links">
+          <div class="links">
+            <ul>
+              <li><a href="http://www.osu.edu/help.php" class="help">Help</a></li>
+              <li><a href="http://buckeyelink.osu.edu/" class="buckeyelink" >BuckeyeLink</a></li>
+              <li><a href="http://www.osu.edu/map/" class="map">Map</a></li>
+              <li><a href="http://www.osu.edu/findpeople.php" class="findpeople">Find People</a></li>
+              <li><a href="https://email.osu.edu/" class="webmail">Webmail</a></li> 
+              <li><a href="http://www.osu.edu/search/" class="search">Search Ohio State</a></li>
+            </ul>
+          </div><!-- /links -->
+        </div><!-- /univ_links -->
+      </div><!-- /container -->
+  </div><!-- /osu_navbar -->
 
   <div id="header" style="background-color: #fff;">
     <div class="container">
